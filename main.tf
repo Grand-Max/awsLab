d0061720a1e692447fa8db6eac6b970e1e5626ab
Max <grandmaxyt@gmail.com>
2023-05-23 00:16:41 +0300

    terraform fix

--- main.tf
+++ main.tf
@@ -1,62 +1,30 @@
+
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.74.0"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-
-  cloud {
-    organization = "spatkiorg"
-
-    workspaces {
-      name = "awsLab"
+      version = "~> 4.16"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1c"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region = "us-east-1"
 }
 
-resource "aws_security_group" "awsLab_sc" {
-  name        = "awsLab_sc"
-  description = "Allow HTTP and SSH traffic via Terraform"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+data "aws_security_group" "selected" {
+  id ="sg-0049eb92d164f56ae"
 }
 
 resource "aws_instance" "app_server" {
-  ami                         = "ami-007855ac798b5175e"
-  instance_type               = "t3.micro"
-  key_name                    = "keyforlab"
-  vpc_security_group_ids      = [aws_security_group.awsLab_sc.id]
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+  key_name 	    = "keyforlab"
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
   associate_public_ip_address = true
-  user_data                   = file("userdata.sh")
+  user_data = file("userdata.sh")
+
   tags = {
-    Name = "awsLab"
+    Name = "aws_lab"
   }
 }
